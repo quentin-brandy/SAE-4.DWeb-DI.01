c2487dc08f8f57fde39e97076f585315fede88cf
quentin brandy <quentin.brandy@protonmail.com>
2024-03-30 17:19:35 +0100

    login update

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
